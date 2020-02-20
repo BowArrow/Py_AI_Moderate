e0a1e2ff39db35e0482478531926e0ff7822c692
nBow <nbow51@gmail.com>
2020-02-20 14:17:57 -0800

    Python AI

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name='WolleyAI',
+    version='0.1dev',
+    packages=['WolleyAI'],
+    license='Creative Commons Attribution-Noncommercial-Share Alike license',
+    long_description=open('README.txt').read()
+)
